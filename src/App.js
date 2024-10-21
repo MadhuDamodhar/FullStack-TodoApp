fe816c1506341a5988b686cde27ac4114334c955
Madhu Damodhar <madhudamodhar9@gmail.com>
2024-10-21 23:01:22 +0530

    updated changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Cards from "./Cards";
 import Service from "./Service";
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
+  const [toggleTheme, setToggleTheme] = useState(true);
+  const [success, setSuccess] = useState('');
+  const [count, setCount] = useState(0);
   const [todo, setTodo] = useState({
     task: "",
     description: "",
+    completed:""
   });
-
   const [tasks, setTasks] = useState([]);
   const [validationError, setValidationError] = useState("");
-
+  const [handleEditOrAdd,setHandleEditOrAdd] = useState(!true) 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setTodo((prevTodo) => ({
@@ -21,77 +23,283 @@ function App() {
     }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleDelete = (id) => {
+    Service.deleteTodoById(id)
+      .then((res) => {
+        setTasks((prevTasks) => prevTasks.filter(task => task.id !== id));
+        setSuccess("success");
+        setValidationError("Deleted successfully");
+        loadTodos();
 
-    if (!todo.task.trim() || !todo.description.trim()) {
-      setValidationError("Please fill out both task and description.");
-      return;
+        setTimeout(() => {
+          setValidationError(""); // Set to empty to hide the message
+        }, 1500);
+      })
+      .catch((err) => {
+        setSuccess("error");
+        setValidationError("Error deleting todo");
+
+        setTimeout(() => {
+          setValidationError("");
+        }, 1500);
+      });
+  };
+  
+  const handleCompleteTask = (id) => {
+    const updatedTasks = tasks.map((task) => 
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+  
+    Service.updateComplte(id, { completed: !tasks.find(task => task.id === id).completed })
+      .then((res) => {
+        setTasks(updatedTasks);
+        const taskName = updatedTasks.find(task => task.id === id).task;
+        
+        if (res.data.completed) {
+          setSuccess("success");
+          setValidationError(`Congrats! Task " ${taskName} " completed. Great job!`);
+        } else {
+          setSuccess("error");
+          setValidationError(`Task " ${taskName} " marked as incomplete. Please review.`);
+        }
+  
+        setTimeout(() => {
+          setValidationError("");
+        }, 1500);
+      })
+      .catch((err) => {
+        console.error(err);
+        setSuccess("error");
+        setValidationError("Error updating task. Please try again.");
+        setTimeout(() => {
+          setValidationError("");
+        }, 1500);
+      });
+  };
+  
+  console.log(todo.completed);
+ 
+  
+    const [todoId,setToodoId]=useState(0);
+    const handleUpdate = ( id )=>{
+if(!handleEditOrAdd){
+  Service.getTodoById(id).then((res)=>{
+    setTodo(res.data)
+    setToodoId(id)
+  }).catch((err)=>{
+    console.log("error while fetching for update");
+  })
+}else{
+  setTodo({
+    task: "",
+    description: ""
+  })
+}
     }
-    
-    Service.addTodo(todo)
-      .then((data) => {
-        setTasks((prevTasks) => [...prevTasks, data]);
-        setTodo({ task: "", description: "" });
+  
+    const handleSave=(e)=>{
+      e.preventDefault();
+      if (!todo.task.trim() || !todo.description.trim()) {
+        setValidationError("Please fill out both task and description.");
+        setSuccess("error");
+        setCount(0)
+        setTodo({
+          task: "",
+          description: ""
+        })
+        setTimeout(() => {
+          setValidationError("");
+        }, 1500);
+        return;
+      }
+      Service.updateTodoById(todoId,todo).then((res)=>{
+        console.log("updated");
+        setValidationError("Updated Task Deatils Successfully.");
+        setSuccess("success");
+        setCount(0)
+        setHandleEditOrAdd(!handleEditOrAdd)
+        setTodo({
+          task: "",
+          description: ""
+        })
+        loadTodos();
+        setTimeout(() => {
+          setValidationError("");
+        }, 1500);
+      }).catch((err)=>{
+        console.log(err);
+      })
+      setTimeout(() => {
         setValidationError("");
-        window.location.reload();
+      }, 1500);
+
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (!todo.task.trim() || !todo.description.trim()) {
+        setValidationError("Please fill out both task and description.");
+        setSuccess("error");
+        setCount(0)
+        setTodo({
+          task: "",
+          description: ""
+        })
+        setTimeout(() => {
+          setValidationError("");
+        }, 1500);
+        return;
+      }
+  
+
+      Service.addTodo(todo)
+        .then((data) => {
+          setTasks((prevTasks) => [...prevTasks, data]);
+          setTodo({ task: "", description: "" });
+          setSuccess("success");
+          setValidationError("Task added successfully !");        
+          setTimeout(() => {
+            setValidationError("");
+          }, 1500);
+          setCount(0);
+          loadTodos();
+        })
+        .catch((err) => {
+          console.error(err);
+          setSuccess("error");
+          setValidationError("Failed to add task. Please try again");
+          setTimeout(() => {
+            setValidationError("");
+          }, 1500);
+        });
+    };
+
+ console.log(todo);
+ 
+
+ 
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  const loadTodos = () => {
+    Service.getTodos()
+      .then((res) => {
+        setTasks(res.data);
+        console.log(res.data);
+        
       })
       .catch((err) => console.error(err));
   };
 
-  useEffect(() => {
-    Service.getTodos()
-      .then((res) => {
-        setTasks(res.data);
-      })
-      .catch((err) => console.error(err));
-  }, []);
+  const handleTheme = () => {
+    setToggleTheme(!toggleTheme);
+    document.body.classList.toggle('dark');
+  };
 
   return (
-    <div className="container-fluid" id="App">
-      <div className="nav">
-        <h1>FullStack-TodoApp</h1>
-      </div>
-      <form
-        id="myForm"
-        className="form-group container"
-        onSubmit={handleSubmit}
-      >
-        <h4>Enter Task details</h4>
-        <input
-          name="task"
-          type="text"
-          placeholder="Task..."
-          className="form-control"
-          value={todo.task}
-          onChange={handleInputChange}
-          autoComplete="off"
-        />
-        <input
-          name="description"
-          type="text"
-          placeholder="Description..."
-          className="form-control"
-          value={todo.description}
-          onChange={handleInputChange}
-          autoComplete="off"
-        />
-        {validationError && <p className="text-danger">{validationError}</p>}
-        <div className="btn1">
-          <button type="submit" className="btn btn-primary">
-            Save Task
+    <div id="container" className="container">
+      <div className="wrapper">
+        <span 
+          style={{ display: validationError ? "block" : "none" }} 
+          id={success === "success" ? "status" : "error"}
+        >
+          {validationError}
+        </span>
+        <div className="header">
+          <div className="fields">
+            <button className="apptitle">
+              Task Master <span><i className="fas fa-tasks"></i></span>
+            </button>
+            {count === 0 ? (
+              <input
+                type="text"
+                name="task"
+                value={todo.task}
+                placeholder="Task name"
+                onChange={handleInputChange}
+              />
+            ) : (
+              <input
+                type="text"
+                name="description"
+                value={todo.description}
+                onChange={handleInputChange}
+                placeholder="Task description"
+              />
+            )}
+
+         {
+          handleEditOrAdd ?(
+
+            <button
+            onClick={(e) => {
+              count === 0 ? setCount(1) : handleSave(e);
+            }}
+            id="update"
+          >
+            {count === 0 ? "Enter" : "Update Task"}
+          </button>
+           
+
+          ):(
+            <button
+            onClick={(e) => {
+              count === 0 ? setCount(1) : handleSubmit(e);
+            }}
+            id="enter"
+          >
+            {count === 0 ? "Enter" : "Add Task"}
+          </button>
+
+          )
+         }
+
+           
+
+
+          </div>
+          <button id="light" onClick={handleTheme}>
+            {toggleTheme ? (
+              <i className="fas fa-sun"></i>
+            ) : (
+              <i className="fas fa-moon"></i>
+            )}
           </button>
         </div>
-      </form>
-      <div className="tasks">
-        {tasks.map((task) => (
-          <Cards
-            key={task.id}
-            id={task.id}
-            todo={task.task}
-            description={task.description}
-          />
-        ))}
+
+        <ul className="body">
+          {tasks && tasks.length > 0 ? (
+            tasks.map((task) => (
+              <li className={task.completed?"markedCard":"card"} key={task.id}>
+                <div className="value">
+                  <h6  style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+                    Name: <span id="taskName">{task.task}</span>
+                  </h6>
+                  <div className="actionsBtn">
+                    <button onClick={()=>{handleCompleteTask(task.id)}}>
+                      <i style={{ color: 'green' }} className={task.completed? "fas fa-times" : "fas fa-check"}></i>
+                    </button>
+                    <button onClick={()=>{handleUpdate(task.id)
+                      setHandleEditOrAdd(!handleEditOrAdd)
+                    }}>
+                      <i style={{ color: 'green' }} className="fas fa-edit"></i>
+                    </button>
+                    <button onClick={() => { handleDelete(task.id) }}>
+                      <i style={{ color: 'red' }} className="fas fa-trash-alt"></i>
+                    </button>
+                  </div>
+                </div>
+                <span style={{ textDecoration: task.completed ? "line-through" : "none" }}   id="description" >
+                Description: <p>{task.description}</p>
+              </span>
+              
+              </li>
+            ))
+          ) : (
+            <h6 id="err" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>No Todos</h6>
+          )}
+        </ul>
       </div>
     </div>
   );
--- src/Service.js
+++ src/Service.js
@@ -1,22 +1,31 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:9191";
+
+// Define your base URL directly in the code
+const BASE_URL = "http://3.110.221.199:9099";
 
 class Service {
   getTodos() {
-    return axios.get(BASE_URL + "/getAllTodos");
+    return axios.get(`${BASE_URL}/getAllTodos`);
   }
-  addTodo(data){
-    return axios.post(BASE_URL + "/addTodos", data);
+
+  addTodo(data) {
+    return axios.post(`${BASE_URL}/addTodos`, data);
   }
-   getTodoById(id){
-    return axios.get(BASE_URL+"/getTodoById/"+ id);
-   }
-   deleteTodoById(id){
-    return axios.delete(BASE_URL+"/deleteTodoById/"+id);
-   }
-   updateTodoById(id, data) {
-     return axios.post(BASE_URL+"/updateTodoById/"+id , data);
-}
+
+  getTodoById(id) {
+    return axios.get(`${BASE_URL}/getTodoById/${id}`);
+  }
+
+  deleteTodoById(id) {
+    return axios.delete(`${BASE_URL}/deleteTodoById/${id}`);
+  }
+
+  updateTodoById(id, data) {
+    return axios.post(`${BASE_URL}/updateTodoById/${id}`, data);
+  }
+  updateComplte(id, data) {
+    return axios.post(`${BASE_URL}/updateComplete/${id}`, data);
+  }
 }
 
 export default new Service();
